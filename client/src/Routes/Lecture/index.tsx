dd916e7b2e9ab5308c1f3147b7e1aded3ea0df61
Tal Mikey <tal.mikey@gmail.com>
2022-05-01 17:57:24 +0300

    feat(facebook): add facebook share will work in prod

--- client/src/Routes/Lecture/index.tsx
+++ client/src/Routes/Lecture/index.tsx
@@ -1,9 +1,9 @@
-import moment from 'moment'
-import { useNavigate, useParams } from 'react-router-dom';
-import {Column, Row} from '../../theme/layout';
-import { Button, Card, CardContent, CardHeader, Chip, Stack, Typography } from '@mui/material';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { Button, Stack, Typography } from '@mui/material';
+import { FacebookShareButton, FacebookIcon } from 'react-share';
+
+import {Row} from '../../theme/layout';
 import lectures from "../../server-mocks/lectures";
-import React from "react";
 import {LectureDetails} from "../../components/LectureDetails";
 
 const Lecture = () => {
@@ -14,12 +14,22 @@ const Lecture = () => {
   const lecture = lectures.find(x => x.id === id);
 
   const subscribeButton = <Button variant='contained' color='secondary' onClick={() => navigate(`subscribe`)}>Subscribe Now</Button>;
+  const author = lecture && `${lecture.lecturer.firstName} ${lecture.lecturer.lastName}`;
+  const facebookQuote = `check out "${lecture?.name}" lecture by ${author}`;
 
   return lecture ?
       <Stack spacing={1}>
+        <Row justifyContent={'end'}>
+          <FacebookShareButton url={window.location.href} quote={facebookQuote}>
+            <Row>
+              <FacebookIcon size={24} round/>
+              <Typography>Share</Typography>
+            </Row>
+          </FacebookShareButton>
+        </Row>
         <Typography variant='h4'>{lecture.name}</Typography>
         <Typography variant='h6' onClick={() => navigate(`../../lecturer/${lecture.lecturer.id}`)} sx={{textDecoration: 'underline', cursor: 'pointer'}}>
-          {`${lecture.lecturer.firstName} ${lecture.lecturer.lastName}`}
+          {author}
         </Typography>
         <LectureDetails lecture={lecture} action={subscribeButton} />
       </Stack> :
